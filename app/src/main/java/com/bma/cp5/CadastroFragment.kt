47ef2238457663b7ef5b26f808f58bf289e65313
aaaaaaaaaana <rm551401@fiap.com.br>
2024-10-04 09:37:06 -0300

    quase hein

---
+++ app/src/main/java/com/bma/cp5/CadastroFragment.kt
@@ -0,0 +1,55 @@
+package com.bma.cp5
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+
+class CadastroFragment : Fragment() {
+
+    private lateinit var btnEnviar: Button
+
+    private var nome:String? = null
+    private var email:String? = null
+    private var idade:String? = null
+    private var senha:String? = null
+    private var confirmarSenha:String? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        nome = arguments?.getString("nome")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_cadastro, container, false)
+
+        btnEnviar = view.findViewById(R.id.btnCadastrar)
+
+
+        btnEnviar.setOnClickListener{
+            nome = arguments?.getString("nome")
+            email = arguments?.getString("email")
+            idade = arguments?.getString("idade")
+            senha = arguments?.getString("senha")
+            confirmarSenha = arguments?.getString("confirmarSenha")
+
+
+            Log.i("Cadastro","Nome:$nome - Email:$email - Idade:$idade - Senha:$senha - Confirmar Senha:$confirmarSenha")
+        }
+
+
+
+
+        return view
+    }
+
+}
--- app/src/main/java/com/bma/cp5/MainActivity.kt
+++ app/src/main/java/com/bma/cp5/MainActivity.kt
@@ -1,20 +1,50 @@
 package com.bma.cp5
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnCadastro: Button
+    private lateinit var btnLista: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        btnCadastro = findViewById(R.id.btnCadastro)
+        btnLista = findViewById(R.id.btnLogin)
+
+        btnCadastro.setOnClickListener{
+            val cadastroFragment = CadastroFragment()
+
+            val bundle = bundleOf(
+                "nome" to "Ana",
+                "idade" to "19",
+                "email" to "ana@gmail.com",
+                "senha" to "moranguinho",
+                "confirmarSenha" to "ana@gmail.com"
+            )
+            cadastroFragment.arguments = bundle
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView,cadastroFragment)
+                .commit()
         }
+
+        btnLista.setOnClickListener{
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView,ListaFragment())
+                .commit()
+        }
+
+
+
+
     }
 }
