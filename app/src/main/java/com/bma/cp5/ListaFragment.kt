3d941024ad4cbcdfd5f166d3fd1c8b43cb1dacfe
aaaaaaaaaana <rm551401@fiap.com.br>
2024-10-06 03:40:59 -0300

    lista quase pronto

--- app/src/main/java/com/bma/cp5/ListaFragment.kt
+++ app/src/main/java/com/bma/cp5/ListaFragment.kt
@@ -1,48 +1,19 @@
 package com.bma.cp5
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.fragment.app.Fragment
 
 class ListaFragment : Fragment() {
 
-//    private lateinit var nome: TextView
-//    private lateinit var email: TextView
-//    private lateinit var idade: TextView
-//    private lateinit var senha: TextView
-//    private lateinit var confirmarSenha: TextView
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        // Infla o layout para este fragmento
-////        val view = inflater.inflate(R.layout.fragment_lista, container, false)
-////
-////        // Obtém as referências para os TextViews
-////        nome = view.findViewById(R.id.nome)
-////        email = view.findViewById(R.id.email)
-////        idade = view.findViewById(R.id.idade)
-////        senha = view.findViewById(R.id.senha)
-////        confirmarSenha = view.findViewById(R.id.confirmarSenha)
-////
-////        // Recupera os dados dos argumentos
-////        val nome = arguments?.getString("nome")
-////        val email = arguments?.getString("email")
-////        val idade = arguments?.getString("idade")
-////        val senha = arguments?.getString("senha")
-////        val confirmarSenha = arguments?.getString("confirmarSenha")
-////
-////        // Define o texto dos TextViews com os dados recebidos
-////        nome.text = nome
-////        email.text = email
-////        idade.text = idade
-////        senha.text = senha
-////        confirmarSenha.text = confirmarSenha
-////
-////        return view
-////    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lista, container, false)
+    }
+
 }
--- app/src/main/java/com/bma/cp5/MainActivity.kt
+++ app/src/main/java/com/bma/cp5/MainActivity.kt
@@ -5,12 +5,10 @@ import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var btnCadastro: Button
-    private lateinit var btnLista: Button
+    private lateinit var btnCadastro:Button
+    private lateinit var btnLogin:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,15 +16,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnCadastro = findViewById(R.id.btnCadastro)
-        btnLista = findViewById(R.id.btnLogin)
+        btnLogin = findViewById(R.id.btnLogin)
 
         btnCadastro.setOnClickListener{
+            val cadastroFragment = CadastroFragment()
+
+            val bundle = bundleOf(
+                "nomeAluno" to "Fabiola",
+                "numFaltas" to 5
+            )
+            cadastroFragment.arguments = bundle
+
             supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentContainerView,CadastroFragment)
+                .replace(R.id.fragmentContainerView,cadastroFragment)
                 .commit()
         }
 
-        btnLista.setOnClickListener{
+        btnLogin.setOnClickListener{
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragmentContainerView,ListaFragment())
                 .commit()
